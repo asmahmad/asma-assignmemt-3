0a970c71d03e99b6d310845a90f578ebcc5cac02
asmahmad <121257988+asmahmad@users.noreply.github.com>
2023-01-24 16:11:16 -0800

    Update UserService.java

--- src/com/coderscampus/assignment3/UserService.java
+++ src/com/coderscampus/assignment3/UserService.java
@@ -7,41 +7,41 @@ import java.io.IOException;
 
 public class UserService {
 	
-	public 	User[] users = new User[4];
+  public 	User[] users = new User[4];
 	
-	public  void createUser() {	
-		BufferedReader reader = null;
-		try {
-			reader = new BufferedReader(new FileReader("data.txt"));
-		} catch (FileNotFoundException e1) {
-			e1.printStackTrace();
-		}	
-		String line;
-		try {
-				int i =0;	
-				while (( line=reader.readLine() ) != null) {
-					String[] fileInput = line.split(",");
-					User newUser = new User(fileInput[0],fileInput[1],fileInput[2]);
-					users[i]  = new User(newUser);
-					i++;
-				}
-		}catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-    public boolean validateUser (String inputUser, String InputPassword) {
-		boolean matchCheck= false;
-		for (int i= 0; i< 4; i++) {
-			if ((users[i].getUserName().equalsIgnoreCase(inputUser)) && (users[i].getPassword().equals(InputPassword))) {
-				System.out.println("Welcome: " + users[i].getName());
-				matchCheck = true;
-				break;
-			}else {
-				matchCheck = false;	
-			}
+  public  void createUser() {	
+	BufferedReader reader = null;
+      try {
+		reader = new BufferedReader(new FileReader("data.txt"));
+	  } catch (FileNotFoundException e1) {
+		e1.printStackTrace();
+	  }	
+	  String line;
+	  try {
+	    int i =0;	
+		while (( line=reader.readLine() ) != null) {
+		  String[] fileInput = line.split(",");
+		  User newUser = new User(fileInput[0],fileInput[1],fileInput[2]);
+		  users[i]  = new User(newUser);
+		  i++;
 	    }
-	  return matchCheck;
-   }
+	  }catch (IOException e) {
+	     e.printStackTrace();
+	  }
+	}
+  public boolean validateUser (String inputUser, String InputPassword) {
+	boolean matchCheck= false;
+	for (int i= 0; i< 4; i++) {
+	  if ((users[i].getUserName().equalsIgnoreCase(inputUser)) && (users[i].getPassword().equals(InputPassword))) {
+		System.out.println("Welcome: " + users[i].getName());
+		matchCheck = true;
+		break;
+	   }else {
+		matchCheck = false;	
+	   }
+    }
+   return matchCheck;
+  }
 }
 
 		
